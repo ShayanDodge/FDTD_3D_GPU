cac1bf1f45cac2725e2a981f2e276220bb8f3887
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2022-06-23 16:26:02 +0430

    Update updateE.m

--- fast/updateE.m
+++ fast/updateE.m
@@ -16,57 +16,16 @@ Psi_ezx_xn= cpml_b1_ex_xn.*Psi_ezx_xn+cpml_a1_ex_xn.*...
 Ey = Ey +CPsi_eyx_xn.*Psi_eyx_xn;
 Ez = Ez +CPsi_ezx_xn.*Psi_ezx_xn; 
 
-Psi_eyx_xp=cpml_b_ex_xp.*Psi_eyx_xp+cpml_a_ex_xp.*...
-    (hz_12-hz_1);
-Psi_ezx_xp=cpml_b1_ex_xp.*Psi_ezx_xp+cpml_a1_ex_xp.*...
-    (hy_12-hy_1);
-
-Ey=Ey+CPsi_eyx_xp.*Psi_eyx_xp;
-Ez=Ez+CPsi_ezx_xp.*Psi_ezx_xp;
-
-Psi_exy_yn =cpml_b_ey_yn.*Psi_exy_yn+cpml_a_ey_yn.*...
-    (hz_22 - hz_2);
-Psi_ezy_yn=cpml_b1_ey_yn.*Psi_ezy_yn+cpml_a1_ey_yn.*...
-    (hx_22-hx_2);
-
-Ex=Ex+ CPsi_exy_yn .* Psi_exy_yn;
-Ez=Ez+ CPsi_ezy_yn .* Psi_ezy_yn; 
-
-Psi_exy_yp=cpml_b_ey_yp.*Psi_exy_yp+cpml_a_ey_yp.*...
-    (hz_22-hz_2);
-Psi_ezy_yp= cpml_b1_ey_yp.*Psi_ezy_yp+cpml_a1_ey_yp.*...
-    (hx_22 - hx_2);
-
-Ex=Ex+CPsi_exy_yp.*Psi_exy_yp;
-Ez=Ez+CPsi_ezy_yp.*Psi_ezy_yp;
-
-Psi_exz_zn =  cpml_b_ez_zn.*Psi_exz_zn+cpml_a_ez_zn.*...
-     (hy_32- hy_3);
-Psi_eyz_zn = cpml_b1_ez_zn.*Psi_eyz_zn+cpml_a1_ez_zn.*...
-    (hx_32 - hx_3);
-
-Ex  =Ex+ CPsi_exz_zn .* Psi_exz_zn;
-Ey = Ey+ CPsi_eyz_zn .* Psi_eyz_zn; 
-
-Psi_exz_zp=cpml_b_ez_zp.*Psi_exz_zp+cpml_a_ez_zp.*...
-     (hy_32 - hy_3);
-Psi_eyz_zp=cpml_b1_ez_zp.*Psi_eyz_zp+cpml_a1_ez_zp.*...
-    (hx_32 - hx_3);
-
-Ex=Ex+CPsi_exz_zp.*Psi_exz_zp;
-Ey=Ey+CPsi_eyz_zp.*Psi_eyz_zp;
- 
-
 Ex=(Cexe.*Ex+Cexhz.*(hz_22-hz_2)+Cexhy.*(hy_32-hy_3)).*SH_1;
-Ey=(Ceye.*Ey+Ceyhx.*(hx_32-hx_3)+Ceyhz.*(hz_12-hz_1)).*SH_2;
-Ez=(Ceze.*Ez+Cezhy.*(hy_12-hy_1)+Cezhx.*(hx_22-hx_2)).*SH_3;
 
-Ex=Ex+source.*exp(-((number.*dt-t_0+0.0075E-9)./tau).^2);
-% sin(2*pi*frequency*number.*dt);
-% exp(-((number.*dt-t_0)./tau).^2);
-% -(sqrt(2*exp(1))/tau)*(number.*dt-t_0).*exp(-((number.*dt-t_0)/tau).^2);
-%cos(2*pi*frequency*(number.*dt-t_0)).*exp(-((number.*dt-t_0)/tau).^2);
+% ************************************
+% ************************************
+% Contact me to see the complete code.
+% Shayan Dodge 
+% dodgeshayan@gmail.com
+% ************************************
+% ************************************
 
-  
+Ex=Ex+source.*exp(-((number.*dt-t_0+0.0075E-9)./tau).^2);  
 end
 
